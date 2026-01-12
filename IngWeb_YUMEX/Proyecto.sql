7ba385b12627d6e35fbc43cc1969159f754e12bb
eduardomartinez4440-byte <eduardomartinez4440@gmail.com>
2026-01-12 12:58:15 -0600

    Add files via upload

--- IngWeb_YUMEX/Proyecto.sql
+++ IngWeb_YUMEX/Proyecto.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `yumexbd` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `yumexbd`;
 -- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: yumexbd
@@ -180,4 +178,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-01-12 12:44:04
+-- Dump completed on 2026-01-12 12:55:23
