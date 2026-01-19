06288131d6e4877f436e94ef98e555b1347d6cc3
LuisSalazarFuentes <128005953+LuisSalazarFuentes@users.noreply.github.com>
2026-01-18 21:54:14 -0600

    Update BD.sql

--- IngWeb_YUMEX/BaseDeDatos/BD.sql
+++ IngWeb_YUMEX/BaseDeDatos/BD.sql
@@ -9,8 +9,11 @@ CREATE TABLE CUENTAS (
     PASSWORD NVARCHAR(100) NOT NULL,
     token_sesion VARCHAR(255),
     -- detalle
-    ROL ENUM('Invitado','Cliente','Administrador') NOT NULL DEFAULT 'Cliente'
+    ROL ENUM('Invitado','Cliente','Administrador','Empleado') NOT NULL DEFAULT 'Cliente',
+    ACTIVO BOOLEAN DEFAULT TRUE
 );
+
+
 
 CREATE TABLE PLATILLOS (
     -- identificadores
@@ -21,7 +24,11 @@ CREATE TABLE PLATILLOS (
     PRECIO DECIMAL(10,2) NOT NULL,
     DISPONIBLE BOOLEAN DEFAULT TRUE,
     DESCRIPCION TEXT,
-    IMAGEN VARCHAR(255)
+    IMAGEN VARCHAR(255),
+    ACTIVO BOOLEAN DEFAULT TRUE,
+    STOCK INT NOT NULL,
+	CATEGORIA VARCHAR(50),
+    TIEMPO_PREPARACION NVARCHAR(50) NOT NULL
 );
 
 CREATE TABLE PEDIDOS (
@@ -31,13 +38,12 @@ CREATE TABLE PEDIDOS (
     FECHA_COMPRA TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     
     -- Tabla de detalles del pedido
-    ID_PLATILLO INT,
-    CANTIDAD INT,
     TOTAL DECIMAL(10,2),
-    
-    -- llaves
-    FOREIGN KEY (ID_PLATILLO) REFERENCES PLATILLOS(ID_PLATILLO),
-    FOREIGN KEY (ID_CUENTA) REFERENCES CUENTAS(ID_CUENTA)
+    ESTADO VARCHAR(20) NOT NULL DEFAULT 'prep',
+    TIPO_PAGO ENUM('efectivo', 'tarjeta') NOT NULL DEFAULT 'efectivo',
+    ID_DIRECCION INT NULL,
+	TIPO_ENTREGA ENUM('LOCAL','DOMICILIO') NOT NULL,
+
 );
 
 
@@ -87,10 +93,66 @@ CREATE TABLE CONTACTO(
     
 );
 
+CREATE TABLE direcciones (
+  ID_DIRECCION INT AUTO_INCREMENT PRIMARY KEY,
+  ID_CUENTA INT NOT NULL,
+  CALLE VARCHAR(100),
+  NUM_EXT VARCHAR(10),
+  NUM_INT VARCHAR(10),
+  COLONIA VARCHAR(100),
+  CP VARCHAR(10),
+  MUNICIPIO VARCHAR(100),
+  CIUDAD VARCHAR(100),
+  PAIS VARCHAR(100),
+  FOREIGN KEY (ID_CUENTA) REFERENCES cuentas(ID_CUENTA)
+);
+
+CREATE TABLE tarjetas (
+  ID_TARJETA INT AUTO_INCREMENT PRIMARY KEY,
+  ID_CUENTA INT NOT NULL,
+  TITULAR VARCHAR(100),
+  NUM_TARJETA VARCHAR(20),
+  FECHA_EXP VARCHAR(10),
+  CVV VARCHAR(5),
+  FOREIGN KEY (ID_CUENTA) REFERENCES cuentas(ID_CUENTA)
+);
+
+
+-- Total de ventas
+SELECT IFNULL(SUM(TOTAL),0) AS ventas FROM PEDIDOS;
+-- Número de pedidos
+SELECT COUNT(*) AS pedidos FROM PEDIDOS;
+-- Ticket promedio
+SELECT IFNULL(AVG(TOTAL),0) AS ticket FROM PEDIDOS;
+-- Artículos en menú
+SELECT COUNT(*) AS menu FROM PLATILLOS;
+
+SELECT 
+  p.NOMBRE,
+  SUM(pd.CANTIDAD) AS total_vendido,
+  SUM(pd.CANTIDAD * pd.PRECIO_UNITARIO) AS total_generado
+FROM PEDIDOS pe
+JOIN PEDIDO_DETALLE pd ON pe.ID_PEDIDO = pd.ID_PEDIDO
+JOIN PLATILLOS p ON pd.ID_PLATILLO = p.ID_PLATILLO
+WHERE MONTH(pe.FECHA_COMPRA) = MONTH(CURRENT_DATE())
+  AND YEAR(pe.FECHA_COMPRA) = YEAR(CURRENT_DATE())
+GROUP BY p.ID_PLATILLO
+ORDER BY total_vendido DESC
+LIMIT 1;
+
+
+
+
+
+
+
+
+
+
 
 -- inserta usuarios iniciales
 INSERT INTO CUENTAS	(NOMBRE, EMAIL ,PASSWORD,ROL) VALUES ('Luis','LuisEnrique@gmail.com', 'Le982895544','Administrador');
-select * from cuentas;
+
 
 INSERT INTO PLATILLOS (NOMBRE, PRECIO, DISPONIBLE, DESCRIPCION, IMAGEN) VALUES ('Barbacoa', 80, TRUE, 'Suave, jugosa y con auténtico sabor tradicional, perfecta para comenzar el día con energía','barbacoa.jpg');
 INSERT INTO PLATILLOS (NOMBRE, PRECIO, DISPONIBLE, DESCRIPCION, IMAGEN) VALUES ('huevos Revueltos',80,TRUE,'Clásicos, esponjosos y recién preparados, ideales para un desayuno rápido y delicioso.','huevos.jpg');
@@ -98,9 +160,28 @@ INSERT INTO PLATILLOS (NOMBRE, PRECIO, DISPONIBLE, DESCRIPCION, IMAGEN) VALUES (
 INSERT INTO PLATILLOS (NOMBRE, PRECIO, DISPONIBLE, DESCRIPCION, IMAGEN) VALUES ('Enchiladas',80,TRUE,'Tortillas suaves rellenas y cubiertas de salsa, crema y queso, con el auténtico toque mexicano','enchiladas.jpg');
 INSERT INTO PLATILLOS (NOMBRE, PRECIO, DISPONIBLE, DESCRIPCION, IMAGEN) VALUES ('Chilaquiles',80,TRUE,'Totopos crujientes bañados en salsa roja o verde, acompañados de crema, queso y el toque casero que amas.','chilquiles.jpg');
 INSERT INTO PLATILLOS (NOMBRE, PRECIO, DISPONIBLE, DESCRIPCION, IMAGEN) VALUES ('Molletes',80,TRUE,'Pan suave con frijoles, queso gratinado y pico de gallo fresco, un desayuno sencillo pero lleno de sabor.','molletes.jpg');
-select * from platillos;
 
-ALTER TABLE CUENTAS ADD ACTIVO BOOLEAN DEFAULT TRUE;
-ALTER TABLE PLATILLOS ADD ACTIVO BOOLEAN DEFAULT TRUE;
-ALTER TABLE pedidos ADD COLUMN ESTADO VARCHAR(20) NOT NULL DEFAULT 'prep';
+ALTER TABLE CUENTAS MODIFY ROL ENUM('Invitado','Cliente','Administrador','Empleado') NOT NULL DEFAULT 'Cliente';
+
+UPDATE CUENTAS SET ROL = 'Cliente' WHERE ROL = 'Empleado';
+ALTER TABLE CUENTAS MODIFY ROL ENUM('Invitado','Cliente','Administrador') NOT NULL DEFAULT 'Cliente';
+ALTER TABLE PEDIDOS MODIFY ESTADO VARCHAR(20) NOT NULL DEFAULT 'prep';
+ALTER TABLE pedidos ADD COLUMN TIPO_PAGO ENUM('efectivo', 'tarjeta') NOT NULL DEFAULT 'efectivo';
+ALTER TABLE platillos ADD STOCK INT NOT NULL DEFAULT 0;
+ALTER TABLE platillos ADD CATEGORIA VARCHAR(50);
+ALTER TABLE platillos ADD TIEMPO_PREPARACION NVARCHAR(50) NOT NULL;
+ALTER TABLE pedidos ADD ID_DIRECCION INT NULL;
+ALTER TABLE pedidos ADD TIPO_ENTREGA ENUM('LOCAL','DOMICILIO') NOT NULL;
+
+
+
+
+select * from cuentas;
+select * from platillos;
 select * from pedidos;
+select * from PEDIDO_DETALLE;
+select * from tarjetas;
+select * from direcciones;
+
+
+
